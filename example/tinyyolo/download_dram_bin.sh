4292a7696bb2d868625244ae7d6fa37fc84942e7
krosac <z1557340316@gmail.com>
2021-10-19 21:02:55 -0700

    frontend generated layer ir and binary weights for tiny yolo

---
+++ example/tinyyolo/download_dram_bin.sh
@@ -0,0 +1,12 @@
+FILEID=1rUoKyqGci8dXKTczYb7DBeEkt2eMZTqE
+FILENAME=weights.bin
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+FILEID=1OVIeASAQjtEkgBMC14JclK90_3QZuaCC
+FILENAME=ins.bin
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+FILEID=1KvHrdGLLytsTX-lPjw8iFqc-U0VIP4tT
+FILENAME=ifm.bin
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+FILEID=1hu7HmWSdIjuZoeYtpTVWWjf_Pw_pzO35
+FILENAME=bias.bin
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
